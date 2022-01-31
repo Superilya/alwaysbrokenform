e65421aecfb4a3ac4db916dcd697d250f52ee575
Ilya Anisimov <imanisimov@alfabank.ru>
2022-02-01 00:37:48 +0300

    update

--- public/index.js
+++ public/index.js
@@ -3,6 +3,34 @@ document.addEventListener('DOMContentLoaded', function() {
     var popup = document.getElementById('modal');
     var header = document.getElementById('header');
     var description = document.getElementById('description');
+    var button = document.getElementById('subButton');
+
+    var inputs = [
+        document.getElementById('name'),
+        document.getElementById('lastName'),
+        document.getElementById('occupation'),
+        document.getElementById('bloodType'),
+        document.getElementById('favoriteSong'),
+        document.getElementById('postCode'),
+        document.getElementById('shoeSize'),
+        document.getElementById('zodiak'),
+        document.getElementById('peniseSize'),
+        document.getElementById('agree'),
+    ];
+
+    form.addEventListener('keyup', function() {
+        for (var i = 0; i < inputs.length; ++i) {
+            var input = inputs[i];
+
+            if (!input.value) {
+                button.setAttribute('disabled', 'true');
+
+                return;
+            }
+        }
+
+        button.removeAttribute('disabled');
+    })
 
     form.onsubmit = function(event) {
         event.preventDefault();
@@ -28,6 +56,13 @@ document.addEventListener('DOMContentLoaded', function() {
     var submitButton = document.getElementById('modal-sub');
 
     submitButton.addEventListener('click', function() {
+        for (var i = 0; i < inputs.length; ++i) {
+            var input = inputs[i];
+
+            input.value = '';
+        }
+
+        button.setAttribute('disabled', 'true');
         popup.classList.remove('show');
     })
 });
